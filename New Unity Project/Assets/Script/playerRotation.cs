938ebf7f21f75ac6c1763286df5bbc6667d78e12
ohsangwon <axur123@gmail.com>
2020-05-11 21:03:15 +0900

    돌면서 발사하는거 깔끔하게 제작 성공

--- New Unity Project/Assets/Script/playerRotation.cs
+++ New Unity Project/Assets/Script/playerRotation.cs
@@ -3,35 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using PathCreation.Examples;
 
-public class playerRotation : MonoBehaviour
+public class PlayerRotation : MonoBehaviour
 {
-    [SerializeField] public PathFollower pathFollower; 
-    [SerializeField] public Vector3 angleman;
-    [SerializeField] public float rotateSpeed;
-    [SerializeField] FixedButton button;
+    //[SerializeField] public PathFollower pathFollower; 
+    //[Range (0f,180f)][SerializeField] public float rotateState = 180f;
+    [SerializeField] public float rotateSpeed = 10f;
+    private Quaternion rotateGoal = Quaternion.Euler(new Vector3(0, 0, 0));
+
     Coroutine courou2;
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            courou2 = StartCoroutine(rotateIt());
+            if(courou2 != null )
+                StopCoroutine(courou2);
+
+            courou2 = StartCoroutine(RotateBack());
         }
         if (Input.GetButtonUp("Fire1"))
         {
-            StopCoroutine(courou2);
+            if (courou2 != null)
+                StopCoroutine(courou2);
+
+            courou2 = StartCoroutine(RotateFront());
+
         }
 
     }
 
 
-    IEnumerator rotateIt() {
+    IEnumerator RotateFront() {
+        rotateGoal = Quaternion.Euler(new Vector3(0, 0, 180));
 
-        print("aa");
-        pathFollower.angleToRotate = Quaternion.Euler(0,180,0) * pathFollower.angleToRotate;
+        while (true)
+        {
+            
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, rotateGoal , rotateSpeed * Time.deltaTime);
+            print(transform.localRotation.z);
+            yield return new WaitForSeconds(0.01f);
+        }
 
-        yield return null;
+    }
+    IEnumerator RotateBack()
+    {
+        print(transform.localRotation.z);
+        rotateGoal = Quaternion.Euler(new Vector3(0, 0, 0));
+        while (true)
+        {
+            print("dwwq");
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, rotateGoal, rotateSpeed * Time.deltaTime);
+            print(transform.localRotation.z);
+         
+            yield return new WaitForSeconds(0.01f);
+        }
+ 
     }
 
-  
+
+
 }
